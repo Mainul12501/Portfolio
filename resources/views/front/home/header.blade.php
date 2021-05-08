b7d64d54211d3a426d3670372828827aa7eeb61c
Muhammad Ali <mainul12501@gmail,com>
2021-05-08 23:32:22 +0600

    'upload'

---
+++ resources/views/front/home/header.blade.php
@@ -0,0 +1,27 @@
+<!--Home-->
+{{--header section--}}
+<section class="section-one" id="home">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div>
+                    <span class="f-s-18">Email: <a href="mailto:mainul125011@gmail.com" class="contact-number-color ">mainul125011@gmail.com</a></span>
+                    <span class="f-s-18 float-right">Phone: <a href="tel:8801646688970" class="contact-number-color">+8801646688970</a></span>
+                </div>
+                <div class="text-center my-5 text-white">
+                    <h1 class="text-white font-italic font-weight-bolder pb-5" style="font-size: 55px; color: royalblue!important;"><strong>Learn & Lead</strong></h1>
+                    <div class=" ">
+                        <img src="{{ asset('/') }}front/img/my-pic.jpg" alt="my-pic" class="circle"/>
+                    </div>
+                    <h5 class="cookie">Oh, hello, nice to meet you!</h5>
+                    <h1 class="text-white cookie" style="font-size: 55px;">
+                        I'm Muhammad Ali (Mainul). <br/>
+                        <strong class="font-weight-bold" style="color: #601fff!important;">Software Engineer and Digital Marketer</strong>
+                    </h1>
+                    <a href="{{ asset('/') }}front/img/CV-it.pdf" class="btn btn-success text-light text-center mx-auto">Download CV</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!--/Home-->
